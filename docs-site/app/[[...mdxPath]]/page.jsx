5159575bd5670405243c0b1b65dffd3ee3f2438a
TriForMine <quentin@triformine.dev>
2026-02-26 18:30:56 +0100

    Include theme MDX components and fallback wrapper

--- docs-site/app/[[...mdxPath]]/page.jsx
+++ docs-site/app/[[...mdxPath]]/page.jsx
@@ -1,25 +1,29 @@
-import { generateStaticParamsFor, importPage } from 'nextra/pages'
-import { useMDXComponents as getMDXComponents } from '../../mdx-components'
+import { generateStaticParamsFor, importPage } from "nextra/pages";
+import { useMDXComponents as getMDXComponents } from "../../mdx-components";
 
-export const generateStaticParams = generateStaticParamsFor('mdxPath')
+export const generateStaticParams = generateStaticParamsFor("mdxPath");
 
 export async function generateMetadata(props) {
-  const params = await props.params
-  const { metadata } = await importPage(params.mdxPath)
-  return metadata
+  const params = await props.params;
+  const { metadata } = await importPage(params.mdxPath);
+  return metadata;
 }
 
-const Wrapper = getMDXComponents().wrapper
+const Wrapper = getMDXComponents().wrapper;
 
 export default async function Page(props) {
-  const params = await props.params
-  const { default: MDXContent, toc, metadata, sourceCode } = await importPage(
-    params.mdxPath
-  )
+  const params = await props.params;
+  const {
+    default: MDXContent,
+    toc,
+    metadata,
+    sourceCode,
+  } = await importPage(params.mdxPath);
+  const MDXWrapper = Wrapper ?? (({ children }) => children);
 
   return (
-    <Wrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
+    <MDXWrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
       <MDXContent {...props} params={params} />
-    </Wrapper>
-  )
+    </MDXWrapper>
+  );
 }
--- docs-site/mdx-components.js
+++ docs-site/mdx-components.js
@@ -1,8 +1,10 @@
-import { useMDXComponents as getNextraMDXComponents } from 'nextra/mdx-components'
+import { useMDXComponents as getNextraMDXComponents } from "nextra/mdx-components";
+import { useMDXComponents as getThemeMDXComponents } from "nextra-theme-docs";
 
 export function useMDXComponents(components) {
   return {
+    ...getThemeMDXComponents(),
     ...getNextraMDXComponents(),
-    ...components
-  }
+    ...components,
+  };
 }
